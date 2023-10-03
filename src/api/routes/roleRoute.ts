54301739e914242a4324bda372bc6539bad1bf08
sairy <10310312-sairy@users.noreply.gitlab.com>
2023-10-03 11:55:47 +0100

    initial commit

---
+++ src/api/routes/roleRoute.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { celebrate, Joi } from 'celebrate';
+
+import { Container } from 'typedi';
+import IRoleController from '../../controllers/IControllers/IRoleController'; 
+
+import config from "../../../config";
+
+const route = Router();
+
+export default (app: Router) => {
+  app.use('/roles', route);
+
+  const ctrl = Container.get(config.controllers.role.name) as IRoleController;
+
+  route.post('',
+    celebrate({
+      body: Joi.object({
+        name: Joi.string().required()
+      })
+    }),
+    (req, res, next) => ctrl.createRole(req, res, next) );
+
+  route.put('',
+    celebrate({
+      body: Joi.object({
+        id: Joi.string().required(),
+        name: Joi.string().required()
+      }),
+    }),
+    (req, res, next) => ctrl.updateRole(req, res, next) );
+};
