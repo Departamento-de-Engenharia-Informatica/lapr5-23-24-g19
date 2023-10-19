e0a5398496799a8da6a49269995b7c6ca27b7dcb
jonasantunes <1181478@isep.ipp.pt>
2023-10-19 19:34:11 +0100

    feat(config): updated config.js

--- config.js
+++ config.js
@@ -1,67 +1,80 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
 // Set the NODE_ENV to 'development' by default
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-const envFound = dotenv.config();
+const envFound = dotenv.config()
 if (!envFound) {
-  // This error should crash whole process
+    // This error should crash whole process
 
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+    throw new Error("⚠️  Couldn't find .env file  ⚠️")
 }
 
 export default {
-  /**
-   * Your favorite port : optional change to 4000 by JRT
-   */
-  port: parseInt(process.env.PORT, 10) || 4000, 
+    /**
+     * Your favorite port : optional change to 4000 by JRT
+     */
+    port: parseInt(process.env.PORT, 10) || 4000,
 
-  /**
-   * That long string from mlab
-   */
-  databaseURL: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/test",
+    /**
+     * That long string from mlab
+     */
+    databaseURL: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test',
 
-  /**
-   * Your secret sauce
-   */
-  jwtSecret: process.env.JWT_SECRET || "my sakdfho2390asjod$%jl)!sdjas0i secret",
+    /**
+     * Your secret sauce
+     */
+    jwtSecret: process.env.JWT_SECRET || 'my sakdfho2390asjod$%jl)!sdjas0i secret',
 
-  /**
-   * Used by winston logger
-   */
-  logs: {
-    level: process.env.LOG_LEVEL || 'info',
-  },
-
-  /**
-   * API configs
-   */
-  api: {
-    prefix: '/api',
-  },
-
-  controllers: {
-    role: {
-      name: "RoleController",
-      path: "../controllers/roleController"
-    }
-  },
-
-  repos: {
-    role: {
-      name: "RoleRepo",
-      path: "../repos/roleRepo"
+    /**
+     * Used by winston logger
+     */
+    logs: {
+        level: process.env.LOG_LEVEL || 'info',
     },
-    user: {
-      name: "UserRepo",
-      path: "../repos/userRepo"
-    }
-  },
 
-  services: {
-    role: {
-      name: "RoleService",
-      path: "../services/roleService"
-    }
-  },
-};
+    /**
+     * API configs
+     */
+    api: {
+        prefix: '/api',
+    },
+
+    controllers: {
+        role: {
+            name: 'RoleController',
+            path: '../controllers/roleController',
+        },
+
+        building: {
+            name: 'BuildingController',
+            path: '../controllers/buildingController',
+        },
+    },
+
+    repos: {
+        role: {
+            name: 'RoleRepo',
+            path: '../repos/roleRepo',
+        },
+        user: {
+            name: 'UserRepo',
+            path: '../repos/userRepo',
+        },
+        building: {
+            name: 'BuildingRepo',
+            path: '../repos/buildingRepo',
+        },
+    },
+
+    services: {
+        role: {
+            name: 'RoleService',
+            path: '../services/roleService',
+        },
+        building: {
+            name: 'BuildingService',
+            path: '../services/buildingService',
+        },
+    },
+}
--- docs/us150/README.md
+++ docs/us150/README.md
@@ -12,4 +12,40 @@ As an administrator, I intend to create a building.
 
 **From the client clarifications:**
 
+
+Caro cliente,
+
+Relativamente à criação de edifícios é suposto criar-mos um edifício sem nenhum piso inicialmente e depois adicionarmos os pisos?
+
+Por exemplo: Criar o edifício A apenas, sem nenhum piso, e só depois na US 190 criar-mos os respetivos pisos do edifício A.
+
+Ou é necessário sempre que criarmos um edifício especificar os pisos que o mesmo tem?
+
+Por exemplo: Criar o edifício A, com os pisos A1, A2, A3 com as dimensões da grelha para cada um dos pisos.
+
+Os melhores cumprimentos,
+
+Grupo 002.
+
+r:boa tarde,
+são dois requisitos independentes. 150 apenas define o edificio. posteriormente o utilizador invocará o caso de uso correspondete ao requisito 190 para criar cada piso desse edificio
+
+Em relação à breve descrição, existe alguma regra em particular?
+
+r:bom dia é opcional, com o máximo de 255 caracteres
+
+Caro cliente,
+
+Será possível esclarecer como funcionarão estas user stories? Com a 230 (Carregar mapa do piso) o nosso entendimento foi que as células seriam carregadas já com a criação de salas e pisos, e assim sendo não faria sentido as outras duas user stories, onde é pedido para criar um piso de um edifício e uma sala. Não entendemos o que é pretendido  com as us's 190 e 310.
+
+Atentamente,
+
+Grupo 63
+
+r:boa tarde,
+o requisito 150 Criar edificio permite criar um edificio, exemplo, edificio "B", com um nome opcional e com uma breve descrição (ex., "departamento de engenharia informática") indicando a dimensão máxima de cada piso em termos de células (ex., 10 x 10)
+o requisito 190 Criar piso permite definir um piso para um dos edificios criados anteriormente, por exemplo, o piso 1 do edificio B com uma breve descrição (ex., "salas TP")
+o requisito 230 Carregar mapa de piso permite ao utlizador fazer upload de um ficheiro descrevendo o mapa de um dado piso. esse ficheiro deve ser validado se tem a estrutura correta e se obdece ao tamanho máximo definido aquando da criação do edificio
+o requisito 310 Criar sala permite definir um sala num dado piso de um edificio, exemplo sala "B310" no 3º piso do edificio B, com uma categorização dessa sala (Gabinete, Anfiteatro, Laboratório, Outro) e uma breve descrição, ex., "Laboratório de Engenharia de Qualidade"
+
 ### 3. Diagrams
