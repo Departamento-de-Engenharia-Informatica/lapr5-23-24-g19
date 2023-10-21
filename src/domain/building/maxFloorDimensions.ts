07c9f968d763917024819c25b475f0c84dfb1b7f
1210954 <123769432+1210954@users.noreply.github.com>
2023-10-21 17:08:39 +0100

    oops

---
+++ src/domain/building/maxFloorDimensions.ts
@@ -0,0 +1,38 @@
+import { ValueObject } from '../../core/domain/ValueObject'
+import { Result } from '../../core/logic/Result'
+import { Guard } from '../../core/logic/Guard'
+
+interface Props {
+    length: number
+    width: number
+}
+
+// not a fan of this name
+export class MaxFloorDimensions extends ValueObject<Props> {
+    get length(): number {
+        return this.props.length
+    }
+
+    get width(): number {
+        return this.props.width
+    }
+
+    private constructor(props: Props) {
+        super(props)
+    }
+
+    public static create(length: number, width: number): Result<MaxFloorDimensions> {
+        const guardResult = Guard.againstNullOrUndefinedBulk([
+            { argument: length, argumentName: 'length' },
+            { argument: width, argumentName: 'width' },
+        ])
+
+        if (!guardResult.succeeded) {
+            return Result.fail(guardResult.message)
+        } else if (length <= 0 || width <= 0) {
+            return Result.fail('Dimensions must have positive values for length and width')
+        } else {
+            return Result.ok(new MaxFloorDimensions({ length, width }))
+        }
+    }
+}
