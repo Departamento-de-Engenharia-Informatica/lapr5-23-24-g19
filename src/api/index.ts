0effbef471a452052ea07e7b3ec0890e2516ab7c
1210954 <123769432+1210954@users.noreply.github.com>
2023-10-21 18:19:18 +0100

    feat(us150): add building router to index

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import auth from './routes/userRoute'
 import user from './routes/userRoute'
 import role from './routes/roleRoute'
+import building from './routes/buildingRoute'
 
 export default () => {
     const app = Router()
@@ -9,6 +10,7 @@ export default () => {
     auth(app)
     user(app)
     role(app)
+    building(app)
 
     return app
 }
--- src/controllers/buildingController.ts
+++ src/controllers/buildingController.ts
@@ -6,24 +6,19 @@ import IBuildingController from './IControllers/IBuildingController'
 import IBuildingService from '../services/IServices/IBuildingService'
 import { IBuildingDTO } from '../dto/IBuildingDTO'
 
-import { Result } from '../core/logic/Result'
-
 @Service()
 export default class BuildingController implements IBuildingController {
     constructor(@Inject(config.services.building.name) private service: IBuildingService) {}
 
     public async createBuilding(req: Request, res: Response, next: NextFunction) {
         try {
-            const buildingOrError = (await this.service.createBuilding(
-                req.body as IBuildingDTO,
-            )) as Result<IBuildingDTO>
+            const result = await this.service.createBuilding(req.body as IBuildingDTO)
 
-            if (buildingOrError.isFailure) {
+            if (result.isFailure) {
                 return res.status(422).send()
             }
 
-            const buildingDTO = buildingOrError.getValue()
-            return res.json(buildingDTO).status(201)
+            return res.json(result.getValue()).status(201);
         } catch (e) {
             return next(e)
         }
