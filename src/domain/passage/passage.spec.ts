0d1b99cd1bf97d52048d87b6e83b091099cf903e
diogonapoles <napolesdr@gmail.com>
2023-10-29 14:35:29 +0000

    test(passages): tested undefined floors

---
+++ src/domain/passage/passage.spec.ts
@@ -0,0 +1,14 @@
+import { assert } from 'chai'
+import { describe, it } from 'mocha'
+import { Passage } from './passage'
+
+describe('Passage', () => {
+    it('floors cannot be undefined', () => {
+        let result = Passage.create({
+            floor1: undefined,
+            floor2: undefined,
+        })
+
+        assert(result.isFailure, 'Undefined floor values should fail');
+    });
+})
