0215702a2d78eba038747d1062a688c65ea5fd8e
1210951 <1210951@isep.ipp.pt>
2023-11-12 20:50:09 +0000

    feat(535): implemented a menu for modules (#72)

---
+++ spa/src/app/components/modules/modules.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModulesComponent } from './modules.component';
+
+describe('AuthComponent', () => {
+  let component: ModulesComponent;
+  let fixture: ComponentFixture<ModulesComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ModulesComponent]
+    });
+    fixture = TestBed.createComponent(ModulesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ spa/src/app/components/modules/modules.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modules',
+  templateUrl: './modules.component.html',
+  styleUrls: ['./modules.component.css']
+})
+export class ModulesComponent {
+
+}
