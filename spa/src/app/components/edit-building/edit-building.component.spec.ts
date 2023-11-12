33b11253af6a2e687cdd12319d970f04e0964e55
1210951 <1210951@isep.ipp.pt>
2023-11-12 20:48:20 +0000

    feat(1070): barebone implementations (#42)

---
+++ spa/src/app/components/edit-building/edit-building.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditBuildingComponent } from './edit-building.component';
+
+describe('EditBuildingComponent', () => {
+  let component: EditBuildingComponent;
+  let fixture: ComponentFixture<EditBuildingComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [EditBuildingComponent]
+    });
+    fixture = TestBed.createComponent(EditBuildingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ spa/src/app/components/edit-building/edit-building.component.ts
@@ -0,0 +1,42 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { BuildingDTO, BuildingService } from 'src/app/services/building.service';
+
+@Component({
+  selector: 'app-edit-building',
+  templateUrl: './edit-building.component.html',
+  styleUrls: ['./edit-building.component.css']
+})
+export class EditBuildingComponent {
+
+  @Output() formSubmitted = new EventEmitter<any>();
+  buildingForm: UntypedFormGroup
+  buildings: BuildingDTO[] = []
+
+
+  constructor(private fb: FormBuilder, private service: BuildingService) {
+    service.getBuildings().subscribe((buildingsList: BuildingDTO[]) => {
+      this.buildings = buildingsList;
+      });
+    this.buildingForm = this.fb.group({
+      selectedBuildingCode: [null, Validators.required],
+      name: [''],
+      description: [''],
+      length: [null, [Validators.required, Validators.min(0)]],
+      width: [null, [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  submitForm() {
+    if (this.buildingForm.valid) {
+      this.formSubmitted.emit(this.buildingForm.value);
+      this.buildingForm.reset();
+    }
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    this.buildingForm.patchValue({ file });
+  }
+
+}
