6e73cb157df8c81dde74f689f1584e44e43db910
1210954 <123769432+1210954@users.noreply.github.com>
2023-11-14 18:08:14 +0000

    feat(us1060): implement search bar with filter

--- spa/src/app/components/get-buildings/get-buildings.component.ts
+++ spa/src/app/components/get-buildings/get-buildings.component.ts
@@ -1,19 +1,43 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, FormGroupDirective, FormGroupName, UntypedFormGroup, Validators } from '@angular/forms';
-import { BuildingService,BuildingDTO } from 'src/app/services/building.service';
+import { Component } from '@angular/core'
+import {
+    BuildingService,
+    BuildingDTO,
+} from 'src/app/services/building.service'
 
 @Component({
-  selector: 'app-get-buildings',
-  templateUrl: './get-buildings.component.html',
-  styleUrls: ['./get-buildings.component.css']
+    selector: 'app-get-buildings',
+    templateUrl: './get-buildings.component.html',
+    styleUrls: ['./get-buildings.component.css'],
 })
 export class GetBuildingsComponent {
-  buildings: BuildingDTO[] = []
-  
-  constructor(private fb: FormBuilder, private service: BuildingService) {
-    service.getBuildings().subscribe((buildingsList: BuildingDTO[]) => {
-      this.buildings = buildingsList;
-      });
-  }
-}
+    private allBuildings: BuildingDTO[] = []
 
+    buildings: BuildingDTO[] = []
+
+    searchFilter?: string
+
+    constructor(private service: BuildingService) {}
+
+    ngOnInit() {
+        this.service.getBuildings().subscribe((buildingsList: BuildingDTO[]) => {
+            this.allBuildings = buildingsList
+            this.buildings = this.allBuildings
+        })
+    }
+
+    filter(event: Event) {
+        const prop = (event.target as HTMLInputElement).value.trim().toLowerCase()
+        if (prop.length === 0) {
+            this.buildings = this.allBuildings
+        } else {
+            this.buildings = this.allBuildings.filter(
+                (b) =>
+                    b.code.toLowerCase().includes(prop) ||
+                    (prop.length > 2 &&
+                        (b.name?.toLowerCase().includes(prop) ||
+                            b.description?.toLowerCase().includes(prop))
+                    ),
+            )
+        }
+    }
+}
