1b78c44fca05a3295842d892feb95a196f2394f3
1210951 <1210951@isep.ipp.pt>
2023-11-15 18:14:00 +0000

    feat(535): added fleet-menu component #72

---
+++ spa/src/app/components/fleet-menu/fleet-menu.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FleetMenuComponent } from './fleet-menu.component';
+
+describe('FleetMenuComponent', () => {
+  let component: FleetMenuComponent;
+  let fixture: ComponentFixture<FleetMenuComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [FleetMenuComponent]
+    });
+    fixture = TestBed.createComponent(FleetMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ spa/src/app/components/fleet-menu/fleet-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fleet-menu',
+  templateUrl: './fleet-menu.component.html',
+  styleUrls: ['./fleet-menu.component.css']
+})
+export class FleetMenuComponent {
+
+}
