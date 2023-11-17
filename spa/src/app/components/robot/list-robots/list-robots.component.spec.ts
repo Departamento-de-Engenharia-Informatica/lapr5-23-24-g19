0a11a7d7839e48201fa1363b39eb6f4d0f348add
jonasantunes <1181478@isep.ipp.pt>
2023-11-17 10:35:21 +0000

    feat(1030): components to list all robots implemented

---
+++ spa/src/app/components/robot/list-robots/list-robots.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListRobotsComponent } from './list-robots.component';
+
+describe('ListRobots', () => {
+  let component: ListRobotsComponent;
+  let fixture: ComponentFixture<ListRobotsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ListRobotsComponent]
+    });
+    fixture = TestBed.createComponent(ListRobotsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ spa/src/app/components/robot/list-robots/list-robots.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { RobotDTO, RobotService } from 'src/app/services/robot.service';
+
+
+@Component({
+    selector: 'app-list-robots',
+    templateUrl: './list-robots.component.html',
+    styleUrls: ['./list-robots.component.css'],
+})
+export class ListRobotsComponent implements OnInit {
+    robots: RobotDTO[];
+
+    constructor(
+        private robotService: RobotService
+    ) {
+        this.robots = [];
+    }
+
+    ngOnInit(): void {
+        this.robotService.getRobots().subscribe((list: RobotDTO[]) => {
+            this.robots = list;
+        });
+    }
+
+
+}
