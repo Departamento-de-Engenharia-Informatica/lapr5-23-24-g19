709f964776976bd93a23c4b6a73b5d7d45739b28
diogonapoles <napolesdr@gmail.com>
2023-11-19 21:01:04 +0000

    test(us1100): e2e tests using cypress (closes #47)

---
+++ spa/cypress/e2e/floors.cy.ts
@@ -0,0 +1,107 @@
+describe('Floors e2e tests', () => {
+    beforeEach(() => {
+        cy.intercept('GET', `http://localhost:4000/api/buildings`, {
+            body: [
+                {
+                    code: 'P',
+                    name: 'Physics',
+                    description: 'Physics Department',
+                    maxFloorDimensions: {
+                        length: 26,
+                        width: 12,
+                    },
+                },
+                {
+                    code: 'C',
+                    name: 'Chemistry',
+                    description: 'Chemistry Department',
+                    maxFloorDimensions: {
+                        length: 20,
+                        width: 15,
+                    },
+                },
+            ],
+        }).as('getBuildings');
+
+        cy.visit('/floors/list');
+    });
+
+    it('has the correct title', () => {
+        cy.title().should('equal', 'List Floors');
+    });
+
+    it('should have an empty selected building', () => {
+        cy.get('#building').should('have.value', null);
+    });
+
+    it('should initially have an empty floor list', () => {
+        cy.get('.floor-card').should('not.exist');
+    });
+
+    it('should select a building and display floors', () => {
+        cy.wait('@getBuildings');
+        cy.intercept('GET', 'http://localhost:4000/api/buildings/P/floors', {
+            body: [
+                {
+                    buildingCode: 'P',
+                    floorNumber: 2,
+                    description: 'Physics Labs',
+                },
+            ],
+        }).as('getFloorsPhysics');
+
+        cy.get('#building').select('P');
+        cy.get('button').contains('Get Floors').click();
+
+        cy.wait('@getFloorsPhysics');
+
+        cy.get('.floor-card').should('exist');
+        cy.get('.floor-card').should('contain.text', 'Floor 2');
+        cy.get('.floor-card').should('contain.text', 'Physics Labs');
+
+        cy.intercept('GET', 'http://localhost:4000/api/buildings/C/floors', {
+            body: [
+                {
+                    buildingCode: 'C',
+                    floorNumber: 1,
+                    description: 'Chemistry Labs',
+                },
+                {
+                    buildingCode: 'C',
+                    floorNumber: 2,
+                    description: 'Chemistry Research',
+                },
+            ],
+        }).as('getFloorsChemistry');
+
+        cy.get('#building').select('C');
+        cy.get('button').contains('Get Floors').click();
+        cy.wait('@getFloorsChemistry');
+
+        cy.get('.floor-card').should('exist');
+        cy.get('.floor-card').should('contain.text', 'Floor 1');
+        cy.get('.floor-card').should('contain.text', 'Chemistry Labs');
+        cy.get('.floor-card').should('contain.text', 'Floor 2');
+        cy.get('.floor-card').should('contain.text', 'Chemistry Research');
+    });
+
+    it('should handle floors with empty descriptions', () => {
+        cy.intercept('GET', 'http://localhost:4000/api/buildings/P/floors', {
+            body: [
+                {
+                    buildingCode: 'P',
+                    floorNumber: 2,
+                    description: '',
+                },
+            ],
+        }).as('getFloorsEmptyDescription');
+
+        cy.get('#building').select('P');
+        cy.get('button').contains('Get Floors').click();
+
+        cy.wait('@getFloorsEmptyDescription');
+
+        cy.get('.floor-card').should('exist');
+        cy.get('.floor-card').should('contain.text', 'No description');
+    });
+});
