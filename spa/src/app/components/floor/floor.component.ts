d06e611c3d084b5e5843f48b8665c6e7d50b752c
diogonapoles <napolesdr@gmail.com>
2023-11-20 15:52:46 +0000

    feat(us1100): integrated use case with parent angular component (#47)

--- spa/src/app/components/floor/floor.component.ts
+++ spa/src/app/components/floor/floor.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import {
+    BuildingDTO,
+    BuildingService,
+} from 'src/app/services/building.service';
 
 @Component({
-  selector: 'app-floor',
-  templateUrl: './floor.component.html',
-  styleUrls: ['./floor.component.css']
+    selector: 'app-floor',
+    templateUrl: './floor.component.html',
+    styleUrls: ['./floor.component.css'],
 })
 export class FloorComponent {
+    selectedBuilding: string;
+    buildings: BuildingDTO[];
 
+    constructor(private buildingService: BuildingService) {
+        this.buildings = [];
+        this.selectedBuilding = '';
+    }
+
+    ngOnInit(): void {
+        this.buildingService.getBuildings().subscribe((list: BuildingDTO[]) => {
+            this.buildings = list;
+        });
+    }
 }
--- spa/src/app/components/floor/list-floors/list-floors.component.ts
+++ spa/src/app/components/floor/list-floors/list-floors.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    Input,
+    OnChanges,
+    OnInit,
+    SimpleChanges,
+} from '@angular/core';
 import {
     BuildingDTO,
     BuildingService,
 } from 'src/app/services/building.service';
-import { FloorAndBuildingDTO, FloorService } from 'src/app/services/floor.service';
+import {
+    FloorAndBuildingDTO,
+    FloorService,
+} from 'src/app/services/floor.service';
 
 @Component({
     selector: 'app-list-floors',
     templateUrl: './list-floors.component.html',
     styleUrls: ['./list-floors.component.css'],
 })
-export class ListFloorsComponent implements OnInit {
-    selectedBuilding: string;
+export class ListFloorsComponent implements OnInit, OnChanges {
+    @Input() selectedBuilding: string;
     floors: FloorAndBuildingDTO[];
+    allFloors: FloorAndBuildingDTO[];
     buildings: BuildingDTO[];
 
     constructor(
@@ -20,6 +30,7 @@ export class ListFloorsComponent implements OnInit {
         private floorService: FloorService,
     ) {
         this.floors = [];
+        this.allFloors = [];
         this.buildings = [];
         this.selectedBuilding = '';
     }
@@ -35,7 +46,34 @@ export class ListFloorsComponent implements OnInit {
         this.floorService
             .getFloors(buildingCode)
             .subscribe((list: FloorAndBuildingDTO[]) => {
-                this.floors = list;
+                this.allFloors = list;
+                this.floors = this.allFloors;
             });
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        this.floors = [];
+        this.floorService
+            .getFloors(this.selectedBuilding)
+            .subscribe((list: FloorAndBuildingDTO[]) => {
+                this.allFloors = list;
+                this.floors = this.allFloors;
+            });
+    }
+
+    filter(event: Event) {
+        const prop = (event.target as HTMLInputElement).value
+            .trim()
+            .toLowerCase();
+        if (prop.length === 0) {
+            this.floors = this.allFloors;
+        } else {
+            this.floors = this.allFloors.filter(
+                (b) =>
+                    b.floorNumber.toString().includes(prop) ||
+                    (prop.length > 2 &&
+                        b.description?.toLowerCase().includes(prop)),
+            );
+        }
     }
 }
