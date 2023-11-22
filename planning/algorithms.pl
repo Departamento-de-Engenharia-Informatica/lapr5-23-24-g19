7d1c8804e2a17b7f31d836f505c26828bde7c492
1210954 <123769432+1210954@users.noreply.github.com>
2023-11-22 18:01:56 +0000

    feat(us510): implement least-buildings sorting criterium

--- planning/algorithms.pl
+++ planning/algorithms.pl
@@ -6,6 +6,7 @@
 ]).
 
 
+:- use_module(library(lists)).
 :- use_module(library(sort)).
 
 % NOTE: Notation used for paths:
@@ -13,21 +14,31 @@
 % PathElement = elev(building, floor1, floor2)
 %             | pass(b1, f1, b2, f2).
 
-resolve_criterium('least-buildings', algorithms:crit2).
 % compute all available criterium
 criteria(Crits) :- findall(Crit, resolve_criterium(Crit, _), Crits).
 
-crit2(_TBD) :- fail.
 
 resolve_criterium('least-elevators', algorithms:less_elevators).
+resolve_criterium('least-buildings', algorithms:less_buildings).
 
+
+% TODO: implement each criteria on it's own file & import it?
 
 less_elevators(Sign, Path1, Path2) :- compare_on(count_elev, Path1, Path2, Sign).
+less_buildings(Sign, Path1, Path2) :- compare_on(count_build, Path1, Path2, Sign).
+
+% TODO: move to a utils/functional.pl file
+map(_, [], []).
+map(Func, [X|Xs], [Y|Ys]) :-
+    call(Func, X, Y),
+    map(Func, Xs, Ys).
+
 % TODO: move to a utils/functional.pl file
 compare_on(Func, X, Y, Sign) :-
     call(Func, X, X1),
     call(Func, Y, Y1),
     compare(Sign, X1, Y1).
+
 
 count_elev(Xs, Count) :- count_elev_aux(Xs, 0, Count).
 count_elev_aux([], Acc, Acc).
@@ -36,6 +47,16 @@ count_elev_aux([elev(_, _, _)|Xs], Acc, Count) :- !,
     count_elev_aux(Xs, NewAcc, Count).
 count_elev_aux([_|Xs], Acc, Count) :- count_elev_aux(Xs, Acc, Count).
 
+
+% not sure if the best impl
+count_build(Path, Count) :-
+    map(extract_build, Path, Builds),
+    flatten(Builds, FlatBuilds),
+    sort(FlatBuilds, Dedups), % HACK: we're sorting only to remove duplicates
+    length(Dedups, Count).
+
+extract_build(elev(B, _, _), B).
+extract_build(pass(B1, _, B2, _), [B1, B2]).
 
 
 
