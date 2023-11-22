5442c3e475520efb166823394e0725d4a7808e50
jonasantunes <1181478@isep.ipp.pt>
2023-11-22 17:12:30 +0000

    feat(us1030): list robots component html changed

--- spa/src/app/components/robot/list-robots/list-robots.component.ts
+++ spa/src/app/components/robot/list-robots/list-robots.component.ts
@@ -8,14 +8,30 @@ import { RobotDTO, RobotService } from 'src/app/services/robot.service'
 })
 export class ListRobotsComponent implements OnInit {
     robots: RobotDTO[]
+    allRobots: RobotDTO[]
 
     constructor(private robotService: RobotService) {
         this.robots = []
+        this.allRobots = []
     }
 
     ngOnInit(): void {
         this.robotService.getRobots().subscribe((list: RobotDTO[]) => {
-            this.robots = list
+            this.allRobots = list
+            this.robots = this.allRobots
         })
+    }
+
+    filter(event: Event) {
+        const prop = (event.target as HTMLInputElement).value.trim().toLowerCase()
+        if (prop.length === 0) {
+            this.robots = this.allRobots
+        } else {
+            this.robots = this.allRobots.filter(
+                (b) =>
+                    b.code.toLowerCase().includes(prop) ||
+                    (prop.length > 2 && b.description?.toLowerCase().includes(prop)),
+            )
+        }
     }
 }
