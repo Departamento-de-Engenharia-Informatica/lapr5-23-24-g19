5cdc10a02693641c942b5e7fb3e908aa4340aabc
1210954 <123769432+1210954@users.noreply.github.com>
2023-11-22 11:17:00 +0000

    feat(planning): added http endpoints

--- planning/http-server.pl
+++ planning/http-server.pl
@@ -3,7 +3,15 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_cors)).
 :- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_client)).
+:- use_module(library(http/http_json)).
+
+:- use_module(library(http/json_convert)).
+
 :- use_module(library(settings)).
+
+:- use_module(library(dicts)).
+
 
 :- set_setting(http:cors, [*]).
 
@@ -16,11 +24,31 @@ stop_server :-
     http_stop_server(Port, _).
 
 
+criteria([ 'least-elevators', 'least-buildings' ]).
+
 :- http_handler('/api/comm-test', comm_test, [ method(get) ]).
 
 comm_test(_Request) :- format('Status: 200~n~n').
 
+:- http_handler('/api/paths/criteria', path_criteria, [ method(get) ]).
 
+path_criteria(_Request) :-
+    criteria(Crit),
+    prolog_to_json(Crit, JSON),
+    reply_json(JSON).
+
+
+% should've been a GET
+% method(post) does not work
+:- http_handler('/api/paths/', get_paths, [method(*)]).
+
+get_paths(Request) :-
+    % http_read_json_dict(Request, Dict),
+    http_read_json_dict(Request, Data),
+
+    prolog_to_json('mandou', JSON),
+
+    reply_json(JSON).
 
 
 
