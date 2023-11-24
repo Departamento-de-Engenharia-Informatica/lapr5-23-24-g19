e7805746f4047a177da8f048990e5aaed43238c4
1210954 <123769432+1210954@users.noreply.github.com>
2023-11-24 23:54:15 +0000

    feat(us510): generate graph edges from floor map

--- planning/loader/floormap.pl
+++ planning/loader/floormap.pl
@@ -2,27 +2,68 @@
     floorcell/4,
     elevator/5,
     passage/6,
+    edge/5,
     loadmap/2
 ]).
 
 % TODO: move to diff module
 getmap(_Building, _Floor, _Map) :- fail.
 
-:- dynamic floorcell/4. % floorcell(building, floor, x, y)
-:- dynamic elevator/5.  % elevator(building, floor, x, y, floors_covered)
-:- dynamic passage/6.   % passage(b_orig, f_orig, x, y, b_dest, f_dest)
+:- dynamic floorcell/4.  % floorcell(building, floor, x, y)
+:- dynamic elevator/5.   % elevator(building, floor, x, y, floors_covered)
+:- dynamic passage/6.    % passage(b_orig, f_orig, x, y, b_dest, f_dest)
+:- dynamic connection/5. % connection(Building, Floor, cell(X1, Y1), cell(X2, Y2), Cost)
+
+edge(Building, Floor, E1, E2, Cost) :-
+    connection(Building, Floor, C1, C2, Cost);
+    connection(Building, Floor, C2, C1, Cost).
 
 loadmap(Building, FLoor) :-
     getmap(Building, FLoor, Map),
-    _{length: Len, width: Wid} = Map.dimensions,
+    _{length: Len, width: Wid} :< Map.dimensions,
+
     populate_cells(Building, FLoor, Len, Wid),
+    build_graph(Building, Floor, Map.mapContent),
 
     populate_passage(Building, Floor, Map.passages),
-    populate_elev(Building, Floor, Map.elevators),
+    populate_elev(Building, Floor, Map.elevators).
 
-    % TODO: add connections
-    fail.
 
+
+build_graph(Building, Floor, FloorMap) :-
+    length(FloorMap, Rows),
+    [H|_] = FloorMap, length(H, Cols),
+
+    Rows1 is Rows-1,
+    Cols1 is Cols-1,
+
+    build_graph_row(Building, Floor, FloorMap, Rows1, Cols1, 0, 0),
+
+    add_diagonals(Building, Floor).
+
+build_graph_row(_, _, [], _, _, _, _) :- !.
+build_graph_row(Building, Floor, [Map|Maps], NRows, NCols, Row, Col) :-
+    build_graph_col(Building, Floor, Map, NRows, NCols, Row, Col),
+    Row1 is Row + 1,
+    build_graph_row(Building, Floor, Maps, NRows, NCols, Row1, Col).
+
+build_graph_col(_, _, [], _, _, _, _) :- !.
+build_graph_col(Building, Floor, [Cell|Cells], NRows, NCols, Row, Col) :-
+    Col1 is Col + 1,
+    Row1 is Row + 1,
+    (
+        ((Cell \== 1); (Cell \== 3)),
+        Col < NCols,
+        assertz(connection(Building, Floor, cell(Row, Col), cell(Row,Col1), 1))
+    ;   true
+    ),
+    (
+        ((Cell \== 2); (Cell \== 3)),
+        Row < NRows,
+        assertz(connection(Building, Floor, cell(Row, Col, 1), cell(Row1,Col, 1), 1))
+    ;   true
+    ),
+    build_graph_col(Building, Floor, Cells, NRows, NCols, Row, Col1).
 
 populate_cells(_, _, 0, _) :- !.
 populate_cells(Building, FLoor, X, Y) :-
