6a8f1c3831e0926bee7154fa36af93f58cd226e9
1210954 <123769432+1210954@users.noreply.github.com>
2023-11-25 19:27:17 +0000

    fix(planning): properly cache requests

--- planning/loader/http-loader.pl
+++ planning/loader/http-loader.pl
@@ -2,7 +2,7 @@
     getmap/3
 ]).
 
-:- dynamic loaded/3. % loaded(Building, Floor, UNIX_time)
+:- dynamic loaded/4. % loaded(Building, Floor, Cache, UNIX_time)
 
 :- use_module(graph, [
     floorcell/4,
@@ -17,14 +17,14 @@ ttl(120).
 
 already_loaded(Building, Floor) :-
     get_time(Now),
-    loaded(Building, Floor, Time),
+    loaded(Building, Floor, _, Time),
 
     ttl(Hold),
     Now - Time =< Hold.
 
 
 getmap(Building, Floor, Map) :-
-    \+ already_loaded(Building, Floor),
+    \+ already_loaded(Building, Floor), !,
 
     % TODO: http connection
     % 1. Ensure MDR URL is defined in the environment
@@ -34,15 +34,18 @@ getmap(Building, Floor, Map) :-
     %    JSON.parse(req.body).map
 
     (
-        (retractall(loaded(Building, Floor, _)); true),
-        (retractall(floorcell(Building, Floor, _, _)); true),
-        (retractall(elevator(Building, Floor, _, _, _)); true),
-        (retractall(passage(Building, Floor, _, _, _, _)); true),
-        (retractall(connection(Building, Floor, _, _, _)); true)
+        (retractall(loaded(Building, Floor, _, _)), !; true),
+        (retractall(floorcell(Building, Floor, _, _)), !; true),
+        (retractall(elevator(Building, Floor, _, _, _)), !; true),
+        (retractall(passage(Building, Floor, _, _, _, _)), !; true),
+        (retractall(connection(Building, Floor, _, _, _)), !; true)
     ),
     get_time(Now),
-    assertz(loaded(Building, Floor, Now)),
+    assertz(loaded(Building, Floor, Map, Now)),
 
     fail.
+
+% grab cache if already_loaded
+getmap(Building, Floor, Map) :- loaded(Building, Floor, Map, _).
 
 % vim: ft=prolog
