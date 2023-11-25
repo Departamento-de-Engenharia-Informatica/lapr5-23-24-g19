0334bb67a35902b7c581e9a57e3ec1f163996d9b
1210954 <123769432+1210954@users.noreply.github.com>
2023-11-25 19:29:16 +0000

    feat(us510): update a* algorithm
    Co-authored-by: jonasantunes <1181478@isep.ipp.pt>

--- planning/algorithms/a-star.pl
+++ planning/algorithms/a-star.pl
@@ -1,37 +1,30 @@
 :- module(algorithms_astar, [
-    a_star/4
+    a_star/5
 ]).
 
-a_star(Orig, Dest, Cam, Custo):-
-    a_star2(Dest, [(_, 0, [Orig])], Cam, Custo).
+a_star(Orig, Dest, EdgePred, Cam, Custo):-
+    a_star2(Dest, [(_, 0, [Orig])], EdgePred, Cam, Custo).
 
-a_star2(Dest, [(_, Custo, [Dest|T])|_], Cam, Custo):-
+a_star2(Dest, [(_, Custo, [Dest|T])|_], _, Cam, Custo):-
 	reverse([Dest|T], Cam).
 
-a_star2(Dest, [(_, Ca, LA)|Outros], Cam, Custo):-
+a_star2(Dest, [(_, Ca, LA)|Outros], EdgePred, Cam, Custo):-
 	LA = [Act|_],
 	findall((CEX, CaX, [X|LA]),
 		(
-            Dest \== Act, edge(Act, X, CustoX), \+ member(X, LA),
-            CaX is CustoX + Ca, estimativa(X, Dest, EstX),
+            Dest \== Act, call(EdgePred, Act, X, CustoX), \+ member(X, LA),
+            CaX is CustoX + Ca, estimate(X, Dest, EstX),
             CEX is CaX + EstX
         ),
         Novos),
 	append(Outros, Novos, Todos),
 	sort(Todos, TodosOrd),
-	a_star2(Dest, TodosOrd, Cam, Custo).
-
-% substituir a chamada edge(Act,X,CustoX)
-% por (edge(Act,X,CustoX);edge(X,Act,CustoX))
-% se quiser ligacoes bidirecionais
+	a_star2(Dest, TodosOrd, EdgePred, Cam, Custo).
 
 
-estimativa(Nodo1, Nodo2, Estimativa) :-
-	node(Nodo1, X1, Y1),
-	node(Nodo2, X2, Y2),
-	Estimativa is sqrt((X1-X2)^2 + (Y1-Y2)^2).
-
-
-
+estimate(Node1, Node2, Estimate) :-
+    Node1 = cell(X1, Y1),
+    Node2 = cell(X2, Y2),
+    Estimate is sqrt((X1-X2)^2 + (Y1-Y2)^2).
 
 % vim: ft=prolog
