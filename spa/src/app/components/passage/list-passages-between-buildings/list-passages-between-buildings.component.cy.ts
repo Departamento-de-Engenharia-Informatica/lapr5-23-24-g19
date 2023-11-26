b6b361a48b3e5cefa5dd4aa8be965d5dbeaab9b0
jonasantunes <1181478@isep.ipp.pt>
2023-11-26 21:27:45 +0000

    tests(us1190): unit tests list PassagesBetweenBuildings

--- spa/src/app/components/passage/list-passages-between-buildings/list-passages-between-buildings.component.cy.ts
+++ spa/src/app/components/passage/list-passages-between-buildings/list-passages-between-buildings.component.cy.ts
@@ -1,21 +1,88 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { FormsModule } from '@angular/forms'
+import { of } from 'rxjs'
+import { BuildingDTO } from 'src/app/dto/BuildingDTO'
+import { BuildingService } from 'src/app/services/building.service'
+import {ListPassagesBetweenBuildingsComponent} from "./list-passages-between-buildings.component";
+import {PassageService} from "../../../services/passage.service";
+import {PassageDTO} from "../../../dto/PassageDTO";
 
-// import { ListPassagesBetweenBuildingsComponent } from './list-passages-between-buildings.component'
+describe('ListPassagesBetweenBuildingsComponent', () => {
+    let buildingServiceStub: Partial<BuildingService>;
+    let passageServiceStub: Partial<PassageService>;
 
-// describe('ListPassagesBetweenBuildingsComponent', () => {
-//     let component: ListPassagesBetweenBuildingsComponent
-//     let fixture: ComponentFixture<ListPassagesBetweenBuildingsComponent>
+    let component: ListPassagesBetweenBuildingsComponent;
+    let fixture: ComponentFixture<ListPassagesBetweenBuildingsComponent>;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({
-//             declarations: [ListPassagesBetweenBuildingsComponent],
-//         })
-//         fixture = TestBed.createComponent(ListPassagesBetweenBuildingsComponent)
-//         component = fixture.componentInstance
-//         fixture.detectChanges()
-//     })
+    const buildings: BuildingDTO[] = [
+        {
+            code: 'A',
+            maxFloorDimensions: { length: 10, width: 10 },
+        },
+        {
+            code: 'B',
+            maxFloorDimensions: { length: 10, width: 10 },
+        },
+    ]
 
-//     it('should create', () => {
-//         expect(component).toBeTruthy()
-//     })
-// })
+    const passages: PassageDTO[] = [
+        {
+            floor1: {
+                buildingCode: 'O',
+                floorNumber: 2,
+            },
+            floor2: {
+                buildingCode: 'P',
+                floorNumber: 3,
+            },
+        },
+    ]
+
+    beforeEach(() => {
+        buildingServiceStub = {
+            getBuildings: function () {
+                return of(buildings)
+            },
+        }
+
+
+        passageServiceStub = {
+            getPassagesBetweenBuildings: function () {
+                return of(passages)
+            },
+        }
+
+        TestBed.configureTestingModule({
+            declarations: [ListPassagesBetweenBuildingsComponent],
+            imports: [FormsModule],
+            providers: [
+                { provide: BuildingService, useValue: buildingServiceStub },
+                { provide: PassageService, useValue: passageServiceStub },
+            ],
+        })
+
+        fixture = TestBed.createComponent(ListPassagesBetweenBuildingsComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
+    })
+
+    it('should create the component', () => {
+        expect(component).to.exist
+    })
+
+    it('should load buildings on init', () => {
+        component.ngOnInit()
+
+        expect(component.buildings).to.eq(buildings)
+    })
+
+    it('should load elevators when getPassagesBetweenBuildings is called', () => {
+
+
+        component.getPassagesBetweenBuildings()
+
+        expect(component.allPassages).to.eq(passages)
+        expect(component.passages).to.eq(passages)
+    })
+})
+
