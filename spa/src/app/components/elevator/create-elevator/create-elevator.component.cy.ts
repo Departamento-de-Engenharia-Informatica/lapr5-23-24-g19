696d79347d3ac3596eceaf11e9c2e8d073f608bb
jonasantunes <1181478@isep.ipp.pt>
2023-11-26 19:39:21 +0000

    tests(elevator): create and list component

--- spa/src/app/components/elevator/create-elevator/create-elevator.component.cy.ts
+++ spa/src/app/components/elevator/create-elevator/create-elevator.component.cy.ts
@@ -1,20 +1,122 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing'
-// import { CreateElevatorComponent } from './create-elevator.component'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { Observable } from 'rxjs'
+import { BuildingDTO } from 'src/app/dto/BuildingDTO'
+import { BuildingService } from 'src/app/services/building.service'
+import { FloorAndBuildingDTO, FloorService } from 'src/app/services/floor.service'
+import {ElevatorService} from "../../../services/elevator.service";
+import {CreateElevatorComponent} from "./create-elevator.component";
+import {CreatedElevatorDTO} from "../../../dto/CreatedElevatorDTO";
 
-// describe('CreateElevatorComponent', () => {
-//     let component: CreateElevatorComponent
-//     let fixture: ComponentFixture<CreateElevatorComponent>
+describe('CreateElevatorComponent: Unit Tests', () => {
+    let buildingServiceStub: Partial<BuildingService>;
+    let floorServiceStub: Partial<FloorService>;
+    let elevatorServiceStub: Partial<ElevatorService>;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({
-//             declarations: [CreateElevatorComponent],
-//         })
-//         fixture = TestBed.createComponent(CreateElevatorComponent)
-//         component = fixture.componentInstance
-//         fixture.detectChanges()
-//     })
+    let component: CreateElevatorComponent;
+    let fixture: ComponentFixture<CreateElevatorComponent>;
 
-//     it('should create', () => {
-//         expect(component).toBeTruthy()
-//     })
-// })
+    const buildings: BuildingDTO[] = [
+        {
+            code: 'A',
+            maxFloorDimensions: { length: 10, width: 10 },
+        },
+        {
+            code: 'B',
+            maxFloorDimensions: { length: 10, width: 10 },
+        },
+    ]
+
+    const floors: FloorAndBuildingDTO[] = [
+        {
+            buildingCode: 'A',
+            floorNumber: 1,
+        },
+        {
+            buildingCode: 'A',
+            floorNumber: 2,
+        },
+    ]
+
+    const elevator3: CreatedElevatorDTO = {
+        buildingId: 'P',
+        identifier: 1,
+        floors: [1],
+        brand: 'a',
+        model: 'a',
+        serialNumber: 'a',
+        description: 'a',
+    }
+
+    beforeEach(() => {
+        buildingServiceStub = {
+            getBuildings: function () {
+                return new Observable<BuildingDTO[]>((observer) => {
+                    observer.next(buildings)
+                    observer.complete()
+                })
+            },
+        }
+
+        floorServiceStub = {
+            getFloors: function () {
+                return new Observable<FloorAndBuildingDTO[]>((observer) => {
+                    observer.next(floors)
+                    observer.complete()
+                })
+            },
+        }
+
+        elevatorServiceStub = {
+            createElevator: function () {
+                return new Observable<CreatedElevatorDTO>((observer) => {
+                    observer.next(elevator3)
+                    observer.complete()
+                })
+            },
+        }
+
+        TestBed.configureTestingModule({
+            declarations: [CreateElevatorComponent],
+            providers: [
+                { provide: BuildingService, useValue: buildingServiceStub },
+                { provide: FloorService, useValue: floorServiceStub },
+                { provide: ElevatorService, useValue: elevatorServiceStub },
+            ],
+        })
+
+        fixture = TestBed.createComponent(CreateElevatorComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
+    })
+
+    it('should create the component', () => {
+        expect(component).to.exist
+    })
+
+    it('should load buildings on init', () => {
+        component.ngOnInit()
+
+        expect(component.buildings).to.eq(buildings)
+    })
+
+    it('should list floors when a building is selected', () => {
+        const selectedBuildingCode = 'A'
+
+        component.selectedBuilding = selectedBuildingCode
+        component.listFloors()
+
+        expect(component.floors).to.eq(floors)
+    })
+
+    it('should reset form and show success alert on successful floor creation', () => {
+        const alertSpy = cy.spy(window, 'alert')
+        const resetSpy = cy.spy(component.createElevatorForm, 'reset')
+
+        component.onSubmit()
+
+        expect(alertSpy).calledWith(
+            `Elevator created successfully!\n`,
+        )
+
+    })
+})
--- spa/src/app/components/elevator/list-elevators/list-elevators.component.cy.ts
+++ spa/src/app/components/elevator/list-elevators/list-elevators.component.cy.ts
@@ -1,21 +1,84 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { FormsModule } from '@angular/forms'
+import { of } from 'rxjs'
+import { BuildingDTO } from 'src/app/dto/BuildingDTO'
+import { BuildingService } from 'src/app/services/building.service'
+import {ElevatorService} from "../../../services/elevator.service";
+import {CreatedElevatorDTO} from "../../../dto/CreatedElevatorDTO";
+import {ListElevatorsComponent} from "./list-elevators.component";
 
-// import { ListElevatorsComponent } from './list-elevators.component'
+describe('ListFloorsComponent', () => {
+    let buildingServiceStub: Partial<BuildingService>;
+    let elevatorServiceStub: Partial<ElevatorService>;
 
-// describe('ListElevatorsComponent', () => {
-//     let component: ListElevatorsComponent
-//     let fixture: ComponentFixture<ListElevatorsComponent>
+    let component: ListElevatorsComponent;
+    let fixture: ComponentFixture<ListElevatorsComponent>;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({
-//             declarations: [ListElevatorsComponent],
-//         })
-//         fixture = TestBed.createComponent(ListElevatorsComponent)
-//         component = fixture.componentInstance
-//         fixture.detectChanges()
-//     })
+    const buildings: BuildingDTO[] = [
+        {
+            code: 'A',
+            maxFloorDimensions: { length: 10, width: 10 },
+        },
+        {
+            code: 'B',
+            maxFloorDimensions: { length: 10, width: 10 },
+        },
+    ]
 
-//     it('should create', () => {
-//         expect(component).toBeTruthy()
-//     })
-// })
+    const elevators: CreatedElevatorDTO[] = [{
+        buildingId: 'P',
+        identifier: 1,
+        floors: [1],
+        brand: 'a',
+        model: 'a',
+        serialNumber: 'a',
+        description: 'a',
+    }]
+
+    beforeEach(() => {
+        buildingServiceStub = {
+            getBuildings: function () {
+                return of(buildings)
+            },
+        }
+
+
+        elevatorServiceStub = {
+            getElevators: function () {
+                return of(elevators)
+            },
+        }
+
+        TestBed.configureTestingModule({
+            declarations: [ListElevatorsComponent],
+            imports: [FormsModule],
+            providers: [
+                { provide: BuildingService, useValue: buildingServiceStub },
+                { provide: ElevatorService, useValue: elevatorServiceStub },
+            ],
+        })
+
+        fixture = TestBed.createComponent(ListElevatorsComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
+    })
+
+    it('should create the component', () => {
+        expect(component).to.exist
+    })
+
+    it('should load buildings on init', () => {
+        component.ngOnInit()
+
+        expect(component.buildings).to.eq(buildings)
+    })
+
+    it('should load elevators when getElevators is called', () => {
+
+
+        component.getElevators()
+
+        expect(component.allElevators).to.eq(elevators)
+        expect(component.elevators).to.eq(elevators)
+    })
+})
