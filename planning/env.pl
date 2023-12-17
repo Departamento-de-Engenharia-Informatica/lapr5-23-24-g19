fdc34c9904853bf2199f4da16b9f0d7d7cfadc6a
1210951 <1210951@isep.ipp.pt>
2023-12-17 23:37:02 +0000

    feat(1430): fixed tasks_cost implementation

--- planning/env.pl
+++ planning/env.pl
@@ -4,7 +4,11 @@
     mdr_url/1,
     elev_cost/1,
     passage_cost/1,
-    task_sequence_alg/1
+    task_sequence_alg/1,
+    num_gen/1,
+    dim_pop/1,
+    prob_cruz/1,
+    prob_mut/1
 ]).
 
 
@@ -31,4 +35,18 @@ task_sequence_alg(Alg) :-
 time_limit(Time) :-
     getenv('TIME_LIMIT', Time), !;
     Time = 60.
+
+    
+num_gen(NG) :-
+    NG = 6.
+    
+dim_pop(DP) :-
+    DP = 4.
+
+prob_cruz(PC) :-
+    PC =50.
+
+prob_mut(PM) :-
+    PM = 50.
+
 % vim: ft=prolog
