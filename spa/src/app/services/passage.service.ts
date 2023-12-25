67159267dfab1b25e0598432a63718eeeeae378b
diogonapoles <napolesdr@gmail.com>
2023-12-25 17:41:50 +0000

    feat(spa): authentication for passages

--- spa/src/app/services/passage.service.ts
+++ spa/src/app/services/passage.service.ts
@@ -1,23 +1,61 @@
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http'
 import { Injectable } from '@angular/core'
-import { Observable, catchError, throwError } from 'rxjs'
+import { Observable, catchError, firstValueFrom, throwError } from 'rxjs'
 import { PassageDTO } from 'src/app/dto/PassageDTO'
 import { BuildingCodePairDTO } from '../dto/BuildingCodePairDTO'
 import { EditPassageDTO } from '../dto/EditPassageDTO'
 import { Config } from '../config'
+import { AuthService } from '@auth0/auth0-angular'
 
 @Injectable({
     providedIn: 'root',
 })
 export class PassageService {
-    constructor(private http: HttpClient) {}
+    constructor(
+        private http: HttpClient,
+        private auth: AuthService,
+    ) {}
+
+    async getToken(): Promise<string> {
+        const tokenObservable = this.auth.getAccessTokenSilently()
+        const token = await firstValueFrom(tokenObservable)
+        return token
+    }
 
     getPassagesBetweenBuildings(
         bCode1: string,
         bCode2: string,
     ): Observable<PassageDTO[]> {
-        const url = `${Config.baseUrl}/passages/?building1=${bCode1}&building2=${bCode2}`
-        return this.http.get<PassageDTO[]>(url, { observe: 'body', responseType: 'json' })
+        return new Observable<PassageDTO[]>((observer) => {
+            this.getToken()
+                .then((token) => {
+                    let params = new HttpParams()
+                    params = params.set('building1', bCode1)
+                    params = params.set('building2', bCode2)
+                    this.http
+                        .get<PassageDTO[]>(`${Config.baseUrl}/passages`, {
+                            params,
+                            headers: {
+                                Authorization: `Bearer ${token}`,
+                                'Content-type': 'application/json',
+                            },
+                            observe: 'body',
+                            responseType: 'json',
+                        })
+                        .subscribe(
+                            (passages) => {
+                                observer.next(passages)
+                                observer.complete()
+                            },
+                            (error) => {
+                                observer.error(error)
+                            },
+                        )
+                })
+                .catch((error) => {
+                    observer.error(error)
+                })
+        })
     }
 
     // Its the same as getPassagesBetweenBuildings but receives a dto instead
@@ -26,47 +64,98 @@ export class PassageService {
         params = params.set('building1', dto.buildingCode1)
         params = params.set('building2', dto.buildingCode2)
 
-        return this.http.get<PassageDTO[]>(`${Config.baseUrl}/passages`, {
-            params,
-            observe: 'body',
-            responseType: 'json',
+        return new Observable<PassageDTO[]>((observer) => {
+            this.getToken()
+                .then((token) => {
+                    this.http
+                        .get<PassageDTO[]>(`${Config.baseUrl}/passages`, {
+                            params,
+                            headers: {
+                                Authorization: `Bearer ${token}`,
+                                'Content-type': 'application/json',
+                            },
+                            observe: 'body',
+                            responseType: 'json',
+                        })
+                        .subscribe(
+                            (passages) => {
+                                observer.next(passages)
+                                observer.complete()
+                            },
+                            (error) => {
+                                observer.error(error)
+                            },
+                        )
+                })
+                .catch((error) => {
+                    observer.error(error)
+                })
         })
     }
 
     patchPassage(dto: EditPassageDTO): Observable<PassageDTO> {
-        return this.http.patch<PassageDTO>(
-            `${Config.baseUrl}/passages`,
-            JSON.stringify(dto),
-
-            {
-                headers: { 'Content-type': 'application/json' },
-                observe: 'body',
-                responseType: 'json',
-            },
-        )
+        return new Observable<PassageDTO>((observer) => {
+            this.getToken()
+                .then((token) => {
+                    this.http
+                        .patch<PassageDTO>(
+                            `${Config.baseUrl}/passages`,
+                            JSON.stringify(dto),
+                            {
+                                headers: {
+                                    Authorization: `Bearer ${token}`,
+                                    'Content-type': 'application/json',
+                                },
+                                observe: 'body',
+                                responseType: 'json',
+                            },
+                        )
+                        .subscribe(
+                            (passage) => {
+                                observer.next(passage)
+                                observer.complete()
+                            },
+                            (error) => {
+                                observer.error(error)
+                            },
+                        )
+                })
+                .catch((error) => {
+                    observer.error(error)
+                })
+        })
     }
 
     postPassage(dto: PassageDTO): Observable<PassageDTO> {
-        return this.http
-            .post<PassageDTO>(`${Config.baseUrl}/passages`, JSON.stringify(dto), {
-                headers: { 'Content-type': 'application/json' },
-                observe: 'body',
-                responseType: 'json',
-            })
-            .pipe(
-                catchError((response: HttpErrorResponse) => {
-                    let errorMessage: string
-
-                    if (response.error) {
-                        // Extract the error message from the response body
-                        errorMessage = response.error
-                    } else {
-                        // If there is no specific error message, use a generic one
-                        errorMessage = `An unexpected error occurred: ${response.message}`
-                    }
-
-                    return throwError(() => new Error(errorMessage))
-                }),
-            )
+        return new Observable<PassageDTO>((observer) => {
+            this.getToken()
+                .then((token) => {
+                    this.http
+                        .post<PassageDTO>(
+                            `${Config.baseUrl}/passages`,
+                            JSON.stringify(dto),
+                            {
+                                headers: {
+                                    Authorization: `Bearer ${token}`,
+                                    'Content-type': 'application/json',
+                                },
+                                observe: 'body',
+                                responseType: 'json',
+                            },
+                        )
+                        .subscribe(
+                            (passage) => {
+                                observer.next(passage)
+                                observer.complete()
+                            },
+                            (error) => {
+                                observer.error(error)
+                            },
+                        )
+                })
+                .catch((error) => {
+                    observer.error(error)
+                })
+        })
     }
 }
