4f6bff64ccdef1e99c7f2f30e214bf380293c887
diogonapoles <napolesdr@gmail.com>
2023-12-25 20:54:45 +0000

    fix(mdr): removed unnecessary controller methods

--- mdr/src/controllers/roleController.ts
+++ mdr/src/controllers/roleController.ts
@@ -7,46 +7,67 @@
 // import IRoleDTO from '../dto/IRoleDTO'
 //
 // import { Result } from '../core/logic/Result'
+
+import { Request, Response, NextFunction } from 'express'
+import { ParamsDictionary } from 'express-serve-static-core'
+import { ParsedQs } from 'qs'
+import { Inject, Service } from 'typedi'
+import config from '../../config'
+import IRoleService from '../services/IServices/IRoleService'
+import IRoleController from './IControllers/IRoleController'
+
 //
-// @Service()
-// export default class RoleController
-//     implements IRoleController /* TODO: extends ../core/infra/BaseController */
-// {
-//     constructor(
-//         @Inject(config.services.role.name) private roleServiceInstance: IRoleService,
-//     ) {}
-//
-//     public async createRole(req: Request, res: Response, next: NextFunction) {
-//         try {
-//             const roleOrError = (await this.roleServiceInstance.createRole(
-//                 req.body as IRoleDTO,
-//             )) as Result<IRoleDTO>
-//
-//             if (roleOrError.isFailure) {
-//                 return res.status(402).send()
-//             }
-//
-//             const roleDTO = roleOrError.getValue()
-//             return res.json(roleDTO).status(201)
-//         } catch (e) {
-//             return next(e)
-//         }
-//     }
-//
-//     public async updateRole(req: Request, res: Response, next: NextFunction) {
-//         try {
-//             const roleOrError = (await this.roleServiceInstance.updateRole(
-//                 req.body as IRoleDTO,
-//             )) as Result<IRoleDTO>
-//
-//             if (roleOrError.isFailure) {
-//                 return res.status(404).send()
-//             }
-//
-//             const roleDTO = roleOrError.getValue()
-//             return res.status(201).json(roleDTO)
-//         } catch (e) {
-//             return next(e)
-//         }
-//     }
-// }
+@Service() /* TODO: extends ../core/infra/BaseController */
+export default class RoleController implements IRoleController {
+    createRole(
+        req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
+        res: Response<any, Record<string, any>>,
+        next: NextFunction,
+    ) {
+        throw new Error('Method not implemented.')
+    }
+    updateRole(
+        req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
+        res: Response<any, Record<string, any>>,
+        next: NextFunction,
+    ) {
+        throw new Error('Method not implemented.')
+    }
+    constructor(
+        @Inject(config.services.role.name) private roleServiceInstance: IRoleService,
+    ) {}
+    //
+    //     public async createRole(req: Request, res: Response, next: NextFunction) {
+    //         try {
+    //             const roleOrError = (await this.roleServiceInstance.createRole(
+    //                 req.body as IRoleDTO,
+    //             )) as Result<IRoleDTO>
+    //
+    //             if (roleOrError.isFailure) {
+    //                 return res.status(402).send()
+    //             }
+    //
+    //             const roleDTO = roleOrError.getValue()
+    //             return res.json(roleDTO).status(201)
+    //         } catch (e) {
+    //             return next(e)
+    //         }
+    //     }
+    //
+    //     public async updateRole(req: Request, res: Response, next: NextFunction) {
+    //         try {
+    //             const roleOrError = (await this.roleServiceInstance.updateRole(
+    //                 req.body as IRoleDTO,
+    //             )) as Result<IRoleDTO>
+    //
+    //             if (roleOrError.isFailure) {
+    //                 return res.status(404).send()
+    //             }
+    //
+    //             const roleDTO = roleOrError.getValue()
+    //             return res.status(201).json(roleDTO)
+    //         } catch (e) {
+    //             return next(e)
+    //         }
+    //     }
+}
