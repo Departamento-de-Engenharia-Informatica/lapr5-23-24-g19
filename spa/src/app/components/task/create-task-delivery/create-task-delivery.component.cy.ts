557370e135332b5afc804f621837138787d6d11a
diogonapoles <napolesdr@gmail.com>
2023-12-29 18:08:08 +0000

    test(us460): spa component testing

--- spa/src/app/components/task/create-task-delivery/create-task-delivery.component.cy.ts
+++ spa/src/app/components/task/create-task-delivery/create-task-delivery.component.cy.ts
@@ -1,7 +1,154 @@
-// import { CreateTaskDeliveryComponent } from './create-task-delivery.component'
-//
-// describe('CreateTaskDeliveryComponent', () => {
-//   it('should mount', () => {
-//     cy.mount(CreateTaskDeliveryComponent)
-//   })
-// })
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ReactiveFormsModule } from '@angular/forms'
+import { AuthService } from '@auth0/auth0-angular'
+import { of } from 'rxjs'
+import { BuildingDTO } from 'src/app/dto/BuildingDTO'
+import { CreateDeliveryTaskDTO } from 'src/app/dto/CreateDeliveryTaskDTO'
+import { CreatedRoomDTO } from 'src/app/dto/CreatedRoomDTO'
+import { BuildingService } from 'src/app/services/building.service'
+import { FloorAndBuildingDTO, FloorService } from 'src/app/services/floor.service'
+import { RoomService } from 'src/app/services/room.service'
+import { TaskService } from 'src/app/services/task.service'
+import { CreateTaskDeliveryComponent } from './create-task-delivery.component'
+
+describe('CreateTaskDeliveryComponent: Unit Tests', () => {
+    let buildingServiceStub: Partial<BuildingService>
+    let floorServiceStub: Partial<FloorService>
+    let roomServiceStub: Partial<RoomService>
+    let taskServiceStub: Partial<TaskService>
+    let authServiceStub: Partial<AuthService>
+
+    let component: CreateTaskDeliveryComponent
+    let fixture: ComponentFixture<CreateTaskDeliveryComponent>
+
+    const buildings: BuildingDTO[] = [
+        {
+            code: 'A',
+            maxFloorDimensions: { length: 10, width: 10 },
+        },
+        {
+            code: 'B',
+            maxFloorDimensions: { length: 10, width: 10 },
+        },
+    ]
+
+    const floors1: FloorAndBuildingDTO[] = [
+        {
+            buildingCode: 'A',
+            floorNumber: 1,
+        },
+        {
+            buildingCode: 'A',
+            floorNumber: 2,
+        },
+    ]
+
+    const rooms1: CreatedRoomDTO[] = [
+        {
+            buildingCode: 'A',
+            floorNumber: 1,
+            name: 'A101',
+            description: 'Room 1',
+            category: 'ROOM',
+            positions: { x: 0, y: 0 },
+            dimensions: { length: 10, width: 10 },
+        },
+        {
+            buildingCode: 'A',
+            floorNumber: 1,
+            name: 'A102',
+            description: 'Room 2',
+            category: 'ROOM',
+            positions: { x: 0, y: 0 },
+            dimensions: { length: 10, width: 10 },
+        },
+    ]
+
+    const task: CreateDeliveryTaskDTO = {
+        email: 'diogo@isep.ipp.pt',
+        startBuildingCode: 'A',
+        startFloorNumber: 1,
+        startRoom: 'A101',
+        goalBuildingCode: 'B',
+        goalFloorNumber: 1,
+        goalRoom: 'B101',
+        pickupContactName: 'Diogo Napoles',
+        pickupContactPhone: '998123123',
+        deliveryContactName: 'Jonas Trindade',
+        deliveryContactPhone: '987654321',
+        confirmationCode: 123456,
+    }
+
+    beforeEach(() => {
+        buildingServiceStub = {
+            getBuildings: function () {
+                return of(buildings)
+            },
+        }
+
+        floorServiceStub = {
+            getFloors: function () {
+                return of(floors1)
+            },
+        }
+
+        roomServiceStub = {
+            getRooms: function () {
+                return of(rooms1)
+            },
+        }
+
+        taskServiceStub = {
+            createDeliveryTask: function () {
+                return of(task)
+            },
+        }
+
+        authServiceStub = {
+            isAuthenticated$: of(true),
+            user$: of({ email: 'diogo@isep.ipp.pt' }),
+        }
+
+        TestBed.configureTestingModule({
+            imports: [ReactiveFormsModule],
+            declarations: [CreateTaskDeliveryComponent],
+            providers: [
+                { provide: BuildingService, useValue: buildingServiceStub },
+                { provide: FloorService, useValue: floorServiceStub },
+                { provide: RoomService, useValue: roomServiceStub },
+                { provide: TaskService, useValue: taskServiceStub },
+                { provide: AuthService, useValue: authServiceStub },
+            ],
+        })
+
+        fixture = TestBed.createComponent(CreateTaskDeliveryComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
+    })
+
+    it('should create the component', () => {
+        expect(component).to.exist
+    })
+
+    it('should load buildings on init', () => {
+        component.ngOnInit()
+        expect(component.buildings).to.eq(buildings)
+    })
+
+    it('should list floors when building is selected', () => {
+        const selectedBuildingCode = 'A'
+        component.selectedBuilding1 = selectedBuildingCode
+
+        component.listFloors1()
+        expect(component.floors1).to.eq(floors1)
+    })
+
+    it('should reset form and show success alert on successful task creation', () => {
+        const resetSpy = cy.spy(component.createDeliveryForm, 'reset')
+
+        component.onSubmit()
+
+        expect(resetSpy).to.be.called
+        expect(component.createDeliveryForm.value.email).to.eq('diogo@isep.ipp.pt')
+    })
+})
--- spa/src/app/components/task/create-task-surveillance/create-task-surveillance.component.cy.ts
+++ spa/src/app/components/task/create-task-surveillance/create-task-surveillance.component.cy.ts
@@ -1,7 +1,117 @@
-// import { CreateTaskComponent } from './create-task.component'
-//
-// describe('CreateTaskComponent', () => {
-//   it('should mount', () => {
-//     cy.mount(CreateTaskComponent)
-//   })
-// })
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { Observable, of } from 'rxjs'
+import { BuildingDTO } from 'src/app/dto/BuildingDTO'
+import { CreateSurveillanceTaskDTO } from 'src/app/dto/CreateSurveillanceTaskDTO'
+import { FloorAndBuildingDTO, FloorService } from 'src/app/services/floor.service'
+import { BuildingService } from 'src/app/services/building.service'
+import { TaskService } from 'src/app/services/task.service'
+import { AuthService } from '@auth0/auth0-angular'
+import { CreateTaskSurveillanceComponent } from './create-task-surveillance.component'
+import { ReactiveFormsModule } from '@angular/forms'
+
+describe('CreateTaskSurveillanceComponent: Unit Tests', () => {
+    let buildingServiceStub: Partial<BuildingService>
+    let floorServiceStub: Partial<FloorService>
+    let taskServiceStub: Partial<TaskService>
+    let authServiceStub: Partial<AuthService>
+
+    let component: CreateTaskSurveillanceComponent
+    let fixture: ComponentFixture<CreateTaskSurveillanceComponent>
+
+    const buildings: BuildingDTO[] = [
+        {
+            code: 'A',
+            maxFloorDimensions: { length: 10, width: 10 },
+        },
+        {
+            code: 'B',
+            maxFloorDimensions: { length: 10, width: 10 },
+        },
+    ]
+
+    const floors: FloorAndBuildingDTO[] = [
+        {
+            buildingCode: 'A',
+            floorNumber: 1,
+        },
+        {
+            buildingCode: 'A',
+            floorNumber: 2,
+        },
+    ]
+
+    const task: CreateSurveillanceTaskDTO = {
+        email: 'diogo@isep.ipp.pt',
+        buildingCode: 'A',
+        floorNumber: 1,
+        contactName: 'Diogo Napoles',
+        contactPhone: '998123123',
+    }
+
+    beforeEach(() => {
+        buildingServiceStub = {
+            getBuildings: function () {
+                return of(buildings)
+            },
+        }
+
+        floorServiceStub = {
+            getFloors: function () {
+                return of(floors)
+            },
+        }
+
+        taskServiceStub = {
+            createSurveillanceTask: function () {
+                return of(task)
+            },
+        }
+
+        authServiceStub = {
+            isAuthenticated$: of(true),
+            user$: of({ email: 'diogo@isep.ipp.pt' }),
+        }
+
+        TestBed.configureTestingModule({
+            imports: [ReactiveFormsModule],
+            declarations: [CreateTaskSurveillanceComponent],
+            providers: [
+                { provide: BuildingService, useValue: buildingServiceStub },
+                { provide: FloorService, useValue: floorServiceStub },
+                { provide: TaskService, useValue: taskServiceStub },
+                { provide: AuthService, useValue: authServiceStub },
+            ],
+        })
+
+        fixture = TestBed.createComponent(CreateTaskSurveillanceComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
+    })
+
+    it('should create the component', () => {
+        expect(component).to.exist
+    })
+
+    it('should load buildings on init', () => {
+        component.ngOnInit()
+
+        expect(component.buildings).to.eq(buildings)
+    })
+
+    it('should list floors when a building is selected', () => {
+        const selectedBuildingCode = 'A'
+        component.selectedBuilding = selectedBuildingCode
+        component.listFloors()
+        expect(component.floors).to.eq(floors)
+    })
+
+    it('should reset form and show success alert on successful task creation', () => {
+        const resetSpy = cy.spy(component.createSurveillanceForm, 'reset')
+
+        component.onSubmit()
+
+        expect(resetSpy).calledWith({
+            email: 'diogo@isep.ipp.pt',
+        })
+    })
+})
