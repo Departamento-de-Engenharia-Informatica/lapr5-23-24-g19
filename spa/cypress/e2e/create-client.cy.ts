861e361275bb2335b89be5b8f1306f73b3eec6b2
jonasantunes <1181478@isep.ipp.pt>
2024-01-02 13:18:30 +0000

    tests(us20): e2e create client tests updated

--- spa/cypress/e2e/create-client.cy.ts
+++ spa/cypress/e2e/create-client.cy.ts
@@ -1,4 +1,4 @@
-function loginViaAuth0Ui(username: string, password: string) {
+/*function loginViaAuth0Ui(username: string, password: string) {
     cy.origin(
         Cypress.env('auth_domain'),
         { args: { username, password } },
@@ -13,60 +13,34 @@ function loginViaAuth0Ui(username: string, password: string) {
                 .click()
         },
     )
-}
+}*/
 
 describe('Client Form e2e tests', () => {
     beforeEach(() => {
-
-        // TODO:NAO ESTA A FUNCIONAR PARA JA POR CAUSA DO cy.visit... assim que tivermos a pagina do create client direita atribuida sera so mudar o link dentro do cy visit
-
-        /*window.localStorage.setItem('USER_ROLES', 'ADM')
-        cy.visit('/create-client')
-
-        const log = Cypress.log({
-            displayName: 'AUTH0 LOGIN',
-            message: [`🔐 Authenticating | ${Cypress.env('auth_username')}`],
-            // @ts-ignore
-            autoEnd: false,
-        })
-        log.snapshot('before')
-
-        loginViaAuth0Ui(Cypress.env('auth_username'), Cypress.env('auth_password'))
-
-        log.snapshot('after')
-        log.end()*/
+        
     })
 
     it('has the correct title', () => {
-        // TODO:NAO ESTA A FUNCIONAR PARA JA POR CAUSA DO titulo, ainda nao atribuido a pagina
-        //cy.title().should('equal', 'Create Client')
+        cy.title().should('equal', '')
     })
 
     it('should have empty initial values', () => {
 
-        //TODO: assim que o resto do codigo em cima estiver operacional remover este cy.visit
         cy.visit('/create-client')
 
-
-
-
-
-        cy.get('#name').should('have.value', '')
+        cy.get('#username').should('have.value', '')
         cy.get('#email').should('have.value', '')
-        cy.get('#phoneNumber').should('have.value', '')
         cy.get('#vatNumber').should('have.value', '')
+        cy.get('#phone').should('have.value', '')
         cy.get('#password').should('have.value', '')
+        cy.get('#confirmPassword').should('have.value', '')
         cy.get('#privacyPolicy').should('not.be.checked');
     })
 
 
     it('should submit the form successfully and display created client', () => {
 
-        //TODO: assim que o resto do codigo em cima estiver operacional remover este cy.visit
         cy.visit('/create-client')
-
-
-
 
         const name = 'quim'
         const email = 'joaquimfontes@isep.ipp.pt'
@@ -74,11 +48,12 @@ describe('Client Form e2e tests', () => {
         const vatNumber = '123789763'
         const password = 'Passw0rd!K'
 
-        cy.get('#name').type(name)
+        cy.get('#username').type(name)
         cy.get('#email').type(email)
-        cy.get('#phoneNumber').type(phoneNumber)
         cy.get('#vatNumber').type(vatNumber)
+        cy.get('#phone').type(phoneNumber)
         cy.get('#password').type(password)
+        cy.get('#confirmPassword').type(password)
         cy.get('#privacyPolicy').check();
 
 
